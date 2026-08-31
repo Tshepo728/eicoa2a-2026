43cd08bd1ed25b3229849bb87da5b3a982e61de0
Tshepo <226062705@edu.vut.ac.za>
2026-08-31 13:38:50 +0200

    Add docstring

---
+++ eicoa2a-2026/unit3/ohms_resistance.py
@@ -0,0 +1,10 @@
+def calc_resistance(voltage ,current):
+    """
+    Calculate Resistance using ohm's law
+    
+    Args:
+        voltage : voltage across the circuit(V).
+        current : current flowing in the circuit(A).Must be non-zero.
+    Returns:
+        float: resistance in ohms
+    """
