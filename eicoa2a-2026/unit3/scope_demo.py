27e310d854f94fdc2d22b74437bf38e8d141ad45
Tshepo <226062705@edu.vut.ac.za>
2026-08-31 13:07:38 +0200

    print function fix

--- eicoa2a-2026/unit3/scope_demo.py
+++ eicoa2a-2026/unit3/scope_demo.py
@@ -3,3 +3,9 @@ def apply_local_threshold(value):
     safety_margin = 2.50
     results = value * safety_margin
     return results
+
+if __name__=="__main__":
+    value = float(input("enter value: "))
+    results = apply_local_threshold(value)
+    print(f"local safety margin = {results :.2f} ")
+    print(f"global safety margin = {safety_margin :.2f} ")
