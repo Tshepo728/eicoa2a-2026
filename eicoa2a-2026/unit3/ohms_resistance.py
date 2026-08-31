c8f34f95424721bb399893c990526e9298285cd1
Tshepo <226062705@edu.vut.ac.za>
2026-08-31 14:01:36 +0200

    rounding to 2d fix

--- eicoa2a-2026/unit3/ohms_resistance.py
+++ eicoa2a-2026/unit3/ohms_resistance.py
@@ -19,4 +19,4 @@ if __name__=="__main__":
     voltage = float(input("enter voltage(V): "))
     current = float(input("enter current(A): "))
     resistance = calc_resistance(voltage ,current)
-    print("Resistance",resistance ,"ohms")
+    print("Resistance",round(resistance,2) ,"ohms")
