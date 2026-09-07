2ac5bb7faf5f4e5c12ff727986b2f3487a0d156d
Tshepo <226062705@edu.vut.ac.za>
2026-09-07 13:37:26 +0200

    UPDATED READINGS

--- eicoa2a-2026/unit4/battery_readings.py
+++ eicoa2a-2026/unit4/battery_readings.py
@@ -1,2 +1,6 @@
 readings = [12.6, 12.4, 12.8, 11.9, 12.5]
 print("ALL READINGS :", readings)
+print("first reading : ", readings[0])
+print("last readding :", readings[-1])
+readings.append(12.7)
+print("UPDATED READINGS :", readings)
