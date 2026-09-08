5f5fe01b08b54dc611afcedfc5d7203578f8c8b0
Tshepo <226062705@edu.vut.ac.za>
2026-09-08 23:21:51 +0200

    ADD 1ST DICTIONARY

---
+++ eicoa2a-2026/unit4/motor_maintance_register.py
@@ -0,0 +1,12 @@
+motors = {
+    "M-101": {
+        "location": "Workshop A",
+        "rated_power": 15,
+        "temperature": 75,
+        "status": "RUNNING",
+        "faults": ["F01", "F02"]
+    },
+}
+
+
+
