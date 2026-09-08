22db1feae1e23557e4dea2bfced5b787b52a8b0f
Tshepo <226062705@edu.vut.ac.za>
2026-09-08 22:08:54 +0200

    add loop and if statements

--- eicoa2a-2026/unit4/battery_dataset_report.py
+++ eicoa2a-2026/unit4/battery_dataset_report.py
@@ -6,3 +6,15 @@ low_readings = []
 voltages = [12.8, 11.4, 12.1, 10.9, 12.7, 11.8, 13.0, 11.2]
 print("highest voltage:", voltages[6])
 print("lowest voltage:", voltages[3])
+
+for voltage in voltages:
+    if voltage >= 12.5:
+        status:"good"
+        good += 1
+    elif voltage >= 11.5:
+        status:"marginal"
+        marginal += 1
+    else:
+        status :"low"
+        low += 1
+        low_readings.append(voltage)
