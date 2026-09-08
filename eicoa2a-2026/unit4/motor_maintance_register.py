1af4ad1a48372ab240862c67d81effacc2f7112d
Tshepo <226062705@edu.vut.ac.za>
2026-09-08 23:35:30 +0200

    dictionaries added

--- eicoa2a-2026/unit4/motor_maintance_register.py
+++ eicoa2a-2026/unit4/motor_maintance_register.py
@@ -1,11 +1,25 @@
 motors = {
     "M-101": {
         "location": "Workshop A",
-        "rated_power": 15,
-        "temperature": 75,
+        "rated_power": 5.5,
+        "temperature": 72,
         "status": "RUNNING",
         "faults": ["F01", "F02"]
     },
+    "M-102": {
+        "location": "Workshop B",
+        "rated_power": 7.5,
+        "temperature": 88,
+        "status": "RUNNING",
+        "faults": ["F02","F03"]
+    },
+    "M-103": {
+        "location": "Pump Station",
+        "rated_power": 11,
+        "temperature": 79,
+        "status": "service required",
+        "faults":["F01","F04"]
+    }
 }
 
 
