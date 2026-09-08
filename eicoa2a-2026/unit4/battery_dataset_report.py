11626d1b76598b701843efaad446cfc6254ac699
Tshepo <226062705@edu.vut.ac.za>
2026-09-08 22:05:11 +0200

    add list,add min and max

---
+++ eicoa2a-2026/unit4/battery_dataset_report.py
@@ -0,0 +1,8 @@
+good = 0
+marginal = 0
+low = 0
+low_readings = []
+
+voltages = [12.8, 11.4, 12.1, 10.9, 12.7, 11.8, 13.0, 11.2]
+print("highest voltage:", voltages[6])
+print("lowest voltage:", voltages[3])
