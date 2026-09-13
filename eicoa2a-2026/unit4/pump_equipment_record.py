b18759f6f165afa04b2d238d4374d5481552ff93
Tshepo <226062705@edu.vut.ac.za>
2026-09-14 00:40:01 +0200

    dictoinary add

---
+++ eicoa2a-2026/unit4/pump_equipment_record.py
@@ -0,0 +1,10 @@
+pump = {
+    "pump_id": "P-104",
+    "location": "workshop B",
+    "rated_power_kw": 7.5,
+    "pressure bar": 4.2,
+    "status": "running"
+
+}
+
+pump["status"] = "service required"
