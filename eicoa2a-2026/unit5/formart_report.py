a0f12f17e8f11959db763cd2476e517e4538c99a
Tshepo <226062705@edu.vut.ac.za>
2026-09-23 14:08:29 +0200

    rounding fixed

--- eicoa2a-2026/unit5/formart_report.py
+++ eicoa2a-2026/unit5/formart_report.py
@@ -1,3 +1,5 @@
 part_name = "  hydraulic-valve-4  "
 pressure = 45.6789
-raw_part_name = part_name.upper().strip().split()
+raw_part_name = part_name.upper().strip().replace("-"," ")
+print("FORMAT REPORT :")
+print(raw_part_name ,"operates at ", round(pressure , 1) ,"bar")
